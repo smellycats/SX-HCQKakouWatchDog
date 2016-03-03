a5437d53072df6de90ddf0aa108a165d14446173
SmellyCat <smellycat2014@foxmail.com>
2016-03-03 14:28:40 +0800

    修复 import json，sms发送修正为post

--- helper_sms.py
+++ helper_sms.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+import json
+
 import requests
+
 
 class SMS(object):
     def __init__(self, **kwargs):
         self.host = kwargs['host']
         self.port = kwargs['port']
-        self.token = kwargs['token']
         self.headers = {'content-type': 'application/json'}
 
     def sms_send(self, content, mobiles):
@@ -13,19 +15,9 @@ class SMS(object):
         url = 'http://{host}:{port}/sms'.format(host=self.host, port=self.port)
         data = {'content': content, 'mobiles': mobiles}
         try:
-            r = requests.get(url, headers=self.headers, data=json.dumps(data))
+            r = requests.post(url, headers=self.headers, data=json.dumps(data))
             if r.status_code == 201:
                 return json.loads(r.text)
         except Exception as e:
-            print e
+            print 'sms:',e
 
-if __name__ == '__main__':
-    sms_ini = {
-        'host': '127.0.0.1',
-        'port': 8090,
-        'username': 'test1',
-        'password': '123456',
-        'token': 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ0MzI1NjcyMiwiaWF0IjoxNDQzMjQ5NTIyfQ.eyJzY29wZSI6WyJzY29wZV9nZXQiLCJoemhiY19nZXQiXSwidWlkIjoyM30.Qga6zksBXBu8Aq9zVBb7tsR_vQFI4A7IfzdgMvGEfrw'
-    }
-    s = SMS(**sms_ini)
-    del s
