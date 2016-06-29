2e98848b97a12deb08cbf0464e7ff1364b271597
SmellyCat <smellycat2014@foxmail.com>
2016-06-29 21:22:42 +0800

    添加 python版本 restful 卡口API

---
+++ helper_kakou2.py
@@ -0,0 +1,42 @@
+﻿# -*- coding: utf-8 -*-
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class Kakou(object):
+    def __init__(self, **kwargs):
+        self.host = kwargs['host']
+        self.port = kwargs['port']
+	self.username = kwargs['username']
+	self.password = kwargs['password']
+        self.headers = {'content-type': 'application/json'}
+
+    def get_kakou_count(self, st, et, kkdd, fxbh):
+        """根据时间,地点,方向获取车流量"""
+        url = 'http://%s:%s/stat?q={"st":"%s","et":"%s","kkbh":"%s","fxbh":"%s"}' % (
+            self.host, self.port, st, et, kkdd, fxbh)
+        print url
+        try:
+            r = requests.get(url, headers=self.headers,
+			     auth=HTTPBasicAuth(self.username, self.password))
+            if r.status_code == 200:
+                return json.loads(r.text)['count']
+        except Exception as e:
+            print e
+            raise
+
+    def get_kkdd(self, kkdd_id):
+        """获取卡口地点"""
+        url = 'http://{0}:{1}/kkdd/{2}'.format(
+            self.host, self.port, kkdd_id)
+        try:
+            r = requests.get(url, headers=self.headers,
+		 	     auth=HTTPBasicAuth(self.username, self.password))
+            if r.status_code == 200:
+                return json.loads(r.text)['items']
+        except Exception as e:
+            print e
+            raise
+
